9e0fe77c03096797cdc0bead426b686ba49c5f6c
Rakesh Chanda <raorakeshc@gmail.com>
2019-12-11 05:32:45 +1100

    Add files via upload

---
+++ src/main/java/com/ntt/myzone/MyZoneApplication.java
@@ -0,0 +1,13 @@
+package com.ntt.myzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyZoneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ntt/myzone/model/AuthResponse.java
@@ -0,0 +1,25 @@
+package com.ntt.myzone.model;
+
+
+public class AuthResponse {
+
+	private String response;
+
+	public AuthResponse() {
+
+	}
+
+	public AuthResponse(String response) {
+		super();
+		this.response = response;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+}
